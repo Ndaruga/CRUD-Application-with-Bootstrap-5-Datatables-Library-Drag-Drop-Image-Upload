6ee9ef871c2f1e88f584a054278c66eec5ecea1c
Ndarugaa <ndaruga.francis@gmail.com>
2023-11-21 10:15:45 +0300

    complete form inputs

--- script.js
+++ script.js
@@ -5,4 +5,24 @@ $(document).ready(function() {
         var url = URL.createObjectURL(file);
         $(this).closest(".row").find(".preview_img").attr("src", url);
     });
+
+    // Display pjone number codes
+    var input = document.querySelector("#phone");
+
+    window.intlTelInput(input, {
+        // show dial codes too
+        separateDialCode: true,
+        
+        // If there are some countries you want to show on the top.
+        preferredCountries: ["ke", "ug"],
+        //Default country
+        initialCountry:"ke",
+        // show only these countres, remove all other
+        // onlyCountries: ["ru", "cn","pk", "sg", "my", "bd"],
+
+        // If there are some countries you want to execlde.
+        // excludeCountries: ["in","il"]
+    });
+
+
 });
