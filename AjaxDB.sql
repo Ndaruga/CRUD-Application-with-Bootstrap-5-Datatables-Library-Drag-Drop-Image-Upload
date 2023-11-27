6a9f1846c8985b9a59577c09c7739c2b66b9cfc6
ndarugaa <ndaruga.francis@gmail.com>
2023-11-27 14:40:33 +0300

    add temp directory

--- AjaxDB.sql
+++ AjaxDB.sql
@@ -9,16 +9,15 @@ CREATE TABLE `User` (
   `First Name` varchar(255) default NULL,
   `Last Name` varchar(255) default NULL,
   `email` varchar(255) default NULL,
-  `phone` varchar(100) default NULL,
   `country` varchar(100) default NULL,
   `Gender` varchar(255) default NULL,
   `Image` TEXT default NULL,
   PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=1;
 
-INSERT INTO `User` (`First Name`,`Last Name`,`email`,`phone`,`country`,`Gender`,`Image`)
+INSERT INTO `User` (`First Name`,`Last Name`,`email`,`country`,`Gender`,`Image`)
 VALUES
-  ("Zenia","Mannix","hendrerit.a@protonmail.com","1-431-863-8420","Kenya","Bo","elit elit fermentum risus, at fringilla purus mauris a"),
-  ("Kelly","Hashim","urna.justo.faucibus@google.ca","1-257-858-8257","Chad","Tarik","sit amet luctus vulputate, nisi sem semper erat,"),
-  ("Randall","Daria","id.enim@yahoo.com","463225-7839","Uganda","Matthew","at, libero. Morbi accumsan laoreet ipsum. Curabitur consequat, lectus"),
-  ("Gabriel","John","eros.non@outlook.couk","1-842-912-1534","Malawi","Jordan","venenatis lacus. Etiam bibendum fermentum metus. Aenean");
+  ("Zenia","Mannix","hendrerit.a@protonmail.com","Kenya","Bo","elit elit fermentum risus, at fringilla purus mauris a"),
+  ("Kelly","Hashim","urna.justo.faucibus@google.ca","Chad","Tarik","sit amet luctus vulputate, nisi sem semper erat,"),
+  ("Randall","Daria","id.enim@yahoo.com","Uganda","Matthew","at, libero. Morbi accumsan laoreet ipsum. Curabitur consequat, lectus"),
+  ("Gabriel","John","eros.non@outlook.couk","Malawi","Jordan","venenatis lacus. Etiam bibendum fermentum metus. Aenean");
